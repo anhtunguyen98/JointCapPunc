a8bd2b716d9741c396fa31969392de300797eca6
anhtunguyen98 <anhtunguyen446@gmail.com>
2022-09-05 16:49:25 +0700

    rm file

--- test.py
+++
@@ -1,7 +0,0 @@
-from nltk import word_tokenize
-from pyvi import ViTokenizer
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained('anhtunguyen98/xlm-base-vi')
-
-print(tokenizer.tokenize("ơibụng"))
